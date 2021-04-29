bf9277f515b233da36018c3dca2cc6a94c9e7a69
graciedevine <gracie.devine@gmail.com>
2021-04-29 10:23:42 -0400

    Added function for graphing 7-day moving average.

--- hr_twitter_bot.py
+++ hr_twitter_bot.py
@@ -1,61 +1,95 @@
 import json
 import tweepy
 import requests
+import pandas as pd
+import matplotlib.pyplot as plt
 import textwrap
-from datetime import datetime, timedelta
-from twitter_keys import consumer_key, consumer_key_secret, access_token, access_token_secret
+from datetime import datetime
+from twitter_keys import (
+    consumer_key,
+    consumer_key_secret,
+    access_token,
+    access_token_secret,
+)
 
-URL = 'https://data.virginia.gov/resource/bre9-aqqr.json'
-LOCALITIES = ['Chesapeake', 'Hampton', 'Newport News', 'Norfolk', 'Portsmouth', 'Suffolk', 'Virginia Beach']
+URL = "https://data.virginia.gov/resource/bre9-aqqr.json"
+LOCALITIES = [
+    "Chesapeake",
+    "Hampton",
+    "Newport News",
+    "Norfolk",
+    "Portsmouth",
+    "Suffolk",
+    "Virginia Beach",
+]
 
-TOTAL = 'total_cases'
-HOSP = 'hospitalizations'
-DEATH = 'deaths'
+TOTAL = "total_cases"
+HOSP = "hospitalizations"
+DEATH = "deaths"
+RDATE = "report_date"
 
-def get_raw_data(from_file = False):
+
+def get_raw_data(from_file=False):
     if from_file:
-        with open('test.json') as f:
-            data_set = json.load(f)
-    else:
-        response = requests.get(URL)
-        data_set = response.json()
+        with open("test.json") as f:
+            return json.load(f)
 
+    response = requests.get(URL)
+    return response.json()
+
+
+def extract_summary_info(raw_data):
+    """Total number of confirmed cases in Hampton Roads."""
     data = dict()
-    for entry in data_set:
-        locality = entry['locality']
+    for entry in raw_data:
+        locality = entry["locality"]
         if locality in LOCALITIES and locality not in data:
             data[locality] = entry
 
-    return data
-
-
-def extract_key_info(data):
-    """Total number of confirmed cases in Hampton Roads."""
-
     fields = [TOTAL, HOSP, DEATH]
-
     return {
-        field: sum([
-            int(info[field]) 
-            for locality, info in data.items()
-        ])
+        field: sum([int(info[field]) for locality, info in data.items()])
         for field in fields
     }
+
 
 def tweet_text(data):
     """Uses daily COVID-19 data to write/format tweet"""
 
-    yesterday = datetime.today().date() - timedelta(days=1)
-    yesterday_str = yesterday.strftime("%b %d")
+    today = datetime.today().date()
+    today_str = today.strftime("%b %d")
 
-    tweet = textwrap.dedent(f'''
-        Total Cases on {yesterday_str}: {data[TOTAL]:,}
-        Hospitalized on {yesterday_str}: {data[HOSP]:,}
-        Deaths on {yesterday_str}: {data[DEATH]:,}
+    tweet = textwrap.dedent(
+        f"""
+        Total Cases as of {today_str}: {data[TOTAL]:,}
+        Hospitalized as of {today_str}: {data[HOSP]:,}
+        Deaths as of {today_str}: {data[DEATH]:,}
 
-         Data taken from: https://www.vdh.virginia.gov/coronavirus/
-    ''').strip()
+        Data taken from: https://www.vdh.virginia.gov/coronavirus/
+    """
+    ).strip()
     return tweet
+
+
+def graph_this_shit(data):
+
+    daily_data = pd.DataFrame(data)
+    for col in [DEATH, HOSP, TOTAL]:
+        daily_data[col] = pd.to_numeric(daily_data[col])
+
+    daily_data[RDATE] = pd.to_datetime(daily_data[RDATE])
+    daily_data = daily_data[daily_data.locality.isin(LOCALITIES)].groupby(RDATE).sum()
+
+    daily_data["New Positive Cases"] = daily_data["total_cases"]
+    daily_data["New 14-day Moving Average"] = (
+        daily_data["New Positive Cases"].rolling(7).mean()
+    )
+    daily_data[["New Positive Cases", "New 14-day Moving Average"]].plot()
+
+    plt.title("DAILY POSITIVE COVID-19 CASES IN VIRGINIA")
+    plt.ylabel("Increase in Positive Cases from Previous Day")
+    plt.xlabel("Dates (last 120 days")
+    plt.show()
 
 
 def send_tweet(text):
@@ -69,15 +103,17 @@ def send_tweet(text):
         print("Authentication OK")
     except:
         print("Error during authentication")
-    
+
     # image = api.media_upload('graph.png')
-    
+
     api.update_status(text)
     # (text, media_ids=[image.media_id, ])
 
-if __name__ == '__main__':
-   data = get_raw_data()
-   data = extract_key_info(data=data)
-   text = tweet_text(data)
-   send_tweet(text)
-   print(text)
+
+if __name__ == "__main__":
+    raw_data = get_raw_data()
+    data = extract_summary_info(raw_data=raw_data)
+    text = tweet_text(data)
+    send_tweet(text)
+    graph_this_shit(raw_data)
+    print(text)
